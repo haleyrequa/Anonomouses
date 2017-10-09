e34d6b4e3779d78c4af10a2bce7fd3f976087b79
hrmr <haleyrequa@gmail.com>
2017-10-09 02:50:54 -0700

    First Commit

---
+++ Anonomous/Assets/Scripts/AI/Entities/PhoneStates.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//todo 
+
+public class PhoneGlobalState : State {
+
+	public override void Enter(Entity entity)
+	{
+	}
+
+	public override void Execute(Entity entity)
+	{
+	}
+
+	public override void Exit(Entity entity)
+	{
+	}
+}
+
+public class PhoneCallingState : State {
+
+	public override void Enter(Entity entity)
+	{
+	}
+
+	public override void Execute(Entity entity)
+	{
+	}
+
+	public override void Exit(Entity entity)
+	{
+	}
+}
---
+++ Anonomous/Assets/Scripts/AI/State.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class State {
+
+	//this will execute when the state is entered
+	public abstract void Enter(Entity GameEntity);
+
+	//this is the states normal update function
+	public abstract void Execute(Entity GameEntity);
+
+
+	//this will execute when the state is exited.
+	public abstract void Exit(Entity GameEntity);
+
+
+}
